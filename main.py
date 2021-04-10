c69da107e3c899835191fb305603a7daf4ba0c25
RafikHachana <70194920+RafikHachana@users.noreply.github.com>
2021-04-11 00:36:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -732,7 +732,7 @@ for s in res:
 
 #if running on heroku
 updater.start_webhook(listen="0.0.0.0",
-                          port=int(PORT),
+                          port=80,
                           url_path=tgbot_token)
 updater.bot.setWebhook('https://inno-happiness-tg-bot.herokuapp.com/' + tgbot_token)
     
